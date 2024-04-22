1220384baf7c427287154f28f896dff39bc1ba5f
train <17704622773@163.com>
2024-04-22 18:07:00 +0800

    feat:vite的配置

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 
 function App() {
-  return <Suspense fallback="loading">{useRoutes(routes)}
-  </Suspense>;
+  return <Suspense fallback="loading">{useRoutes(routes)}</Suspense>;
 }
 
 export default App;
