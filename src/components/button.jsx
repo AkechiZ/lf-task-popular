53e2503a2ba043f7f4ad8cf89e584548f08dfea9
train <17704622773@163.com>
2024-04-26 15:52:51 +0800

    feat: 更改测试

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,7 +2,7 @@ import './index.scss';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { LoadingOutlined } from '@ant-design/icons';
 
-export function Button({ loading, children, ...restProps }) {
+  export function Button({ loading, children, ...restProps }) {
   return (
     <button type="button" className="button" {...restProps}>
       {loading && (
